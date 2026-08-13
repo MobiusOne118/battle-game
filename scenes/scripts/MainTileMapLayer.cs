308212b59b07835d627c82b51261a597be15f943
MobiusOne118 <44667250+MobiusOne118@users.noreply.github.com>
2026-08-13 16:47:40 +1000

    Basic terrain highlight and first pass on units (#1)

--- scenes/scripts/MainTileMapLayer.cs
+++ scenes/scripts/MainTileMapLayer.cs
@@ -1,18 +1,40 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 public partial class MainTileMapLayer : TileMapLayer
 {
+	[Signal] public delegate void CellClickedEventHandler(Vector2I cell);
 	[Export] private TileMapLayer _hoverLayer;
+	[Export] private TileMapLayer _selectionLayer;
+	[Export] private PackedScene _unitScene;
+	[Export] private int _unitCount { get; set; } = 3;
+	
+	private Node2D _unitsContainer;
 	private Vector2I? _hoveredCell = null;
+	private Vector2I? _selectedCell = null;
+	private List<BaseUnit> _units = new List<BaseUnit>();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		if (_hoverLayer == null)
-        GD.PrintErr("HexMap: _hoverLayer is not assigned in the editor!");
-	}
+			GD.PrintErr("HexMap: _hoverLayer is not assigned in the editor!");
 
+		_unitsContainer = GetTree().Root.GetNode<Node2D>("Main/UnitsContainer");
+
+		for (int i = 0; i < _unitCount; i++)
+		{
+			var unit = _unitScene.Instantiate<BaseUnit>();
+			_unitsContainer.AddChild(unit);
+			_units.Add(unit);
+		}
+		// SpawnUnit(new Vector2I(0,1));
+
+		DeploymentPhase();
+	}
+	
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
@@ -31,10 +53,9 @@ public partial class MainTileMapLayer : TileMapLayer
 		if (GetCellSourceId(cell) != -1)
 		{
 			_hoveredCell = cell;
-			GD.Print("Cell: " + _hoveredCell + " | SourceId: " + GetCellSourceId(cell) + " | CellAtlas: " + GetCellAtlasCoords(cell));
+			// GD.Print("Cell: " + _hoveredCell + " | SourceId: " + GetCellSourceId(cell) + " | CellAtlas: " + GetCellAtlasCoords(cell));
 			// _hoverLayer.SetCell(cell, GetCellSourceId(cell), GetCellAtlasCoords(cell));
 			_hoverLayer.SetCell(cell, 1, new Vector2I(0,0));
-			GD.Print("HoverLayer Count: " + _hoverLayer.GetUsedCells().Count);
 		}
 		else
 		{
@@ -43,9 +64,81 @@ public partial class MainTileMapLayer : TileMapLayer
 
 	}
 
+	//
+	// Highlight selected cell logic
+	//
+	private void SelectCell(Vector2I cell)
+	{
+		_selectedCell = cell;
+		// Clear previous selection
+    _selectionLayer.Clear();
+    
+    // Paint new selection
+    _selectionLayer.SetCell(cell, 1, new Vector2I(0, 0));
+	}
+	private void SelectClear()
+	{
+		_selectedCell = null;
+		_selectionLayer.Clear();
+	}
 
-	// Leaving here for now, maybe recycle these for battle scaring
-	/*private void HighlightCell(Vector2I cell)
+	//
+	// Unit Spawn
+	//
+	private void SpawnUnit(Vector2I cell)
+	{
+		GD.Print("Spawning unit at " + cell);
+		var unit = _unitScene.Instantiate<BaseUnit>();
+    _unitsContainer.AddChild(unit);
+    unit.Init(cell, this);
+	}
+
+	//
+	// User interaction logic
+	//
+  public override void _Input(InputEvent @event)
+  {
+    if (@event is InputEventMouseButton mouseEvent
+			&& mouseEvent.ButtonIndex == MouseButton.Left
+			&& mouseEvent.Pressed)
+		{
+			Vector2I cell = LocalToMap(ToLocal(mouseEvent.Position));
+
+			if (GetCellSourceId(cell) != -1)
+			{
+				// GD.Print("Selected cell: " + cell);
+				SelectCell(cell);
+				EmitSignal(SignalName.CellClicked, cell);
+			}
+		}
+
+		if (@event is InputEventMouseButton rightClickEvent
+			&& rightClickEvent.ButtonIndex == MouseButton.Right
+			&& rightClickEvent.Pressed)
+		{
+			SelectClear();
+		}
+  }
+	
+	public async Task<Vector2I> AwaitCellClick()
+	{
+		var result = await ToSignal(this, SignalName.CellClicked);
+		return (Vector2I)result[0];
+	}
+	public async void DeploymentPhase()
+	{
+		foreach (var unit in _units)
+		{
+			GD.Print("Deploy unit");
+			Vector2I cell = await AwaitCellClick();
+			unit.Init(cell, this);
+		}
+	}
+
+	// Leaving here for now
+	// maybe recycle these for battle scaring
+	/*
+	private void HighlightCell(Vector2I cell)
 	{
 		int sourceId = GetCellSourceId(cell);
 		Vector2I atlasCoords = GetCellAtlasCoords(cell);
@@ -60,17 +153,6 @@ public partial class MainTileMapLayer : TileMapLayer
 
 		Vector2I atlasCoords = GetCellAtlasCoords(cell);
 		SetCell(cell, sourceId, atlasCoords, NormalAltId);
-	}*/
-
-  public override void _UnhandledInput(InputEvent @event)
-  {
-    if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
-		{
-			// Fetch our mouse position and hex tile
-			// var local_pos = _tileMapLayer.GetLocalMousePosition();
-			var global_pos = GetGlobalMousePosition();
-			GD.Print("Mouse Pos: " + global_pos);
-		}
-  }
-
+	}
+	*/
 }
